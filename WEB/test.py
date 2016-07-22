28f8723e0b838eb9a77e0127ac2f5b17257ac7bb
Haonan Li <haonanli@6tiantian.com>
2016-07-22 21:49:30 +0800

    save

--- WEB/test.py
+++ WEB/test.py
@@ -1,5 +1,6 @@
 import tornado.ioloop
 import tornado.web
+import os
 
 
 class MainHandler(tornado.web.RequestHandler):
@@ -8,8 +9,12 @@ class MainHandler(tornado.web.RequestHandler):
 
 
 if __name__ == "__main__":
+    settings = {
+        "static_path": os.path.join(os.path.dirname(__file__), "static")
+    }
+
     application = tornado.web.Application([
         (r"/", MainHandler),
-    ])
+    ], **settings)
     application.listen(8888)
     tornado.ioloop.IOLoop.current().start()
